2f468aaa2030c120dcf582c5cb8cbe89f7d03438
unknown <charang2003@gmail.com>
2024-07-30 23:00:03 +0530

    missing number

---
+++ 53.cpp
@@ -0,0 +1,25 @@
+// maximum subarray
+
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        int n = arr.size();
+        int ans = nums[0];
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum = sum + nums[i];
+            if (ans < sum)
+            {
+                ans = sum;
+            }
+            if (sum < 0)
+            {
+                sum = 0;
+            }
+        }
+        return ans;
+    }
+};
