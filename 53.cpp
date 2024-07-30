2b2f99e86b8ccd6d9d10e439b6facb1f25d017a7
unknown <charang2003@gmail.com>
2024-07-30 23:01:29 +0530

    maximum subarray

--- 53.cpp
+++ 53.cpp
@@ -5,10 +5,10 @@ class Solution
 public:
     int maxSubArray(vector<int> &nums)
     {
-        int n = arr.size();
+        int size = arr.size();
         int ans = nums[0];
         int sum = 0;
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < size; i++)
         {
             sum = sum + nums[i];
             if (ans < sum)
